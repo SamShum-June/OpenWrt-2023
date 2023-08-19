9dde4df857e054d0028436a47190d338c78c86f1
SamShum_June <38761558+SamShum-June@users.noreply.github.com>
2023-08-19 19:13:45 +0800

    diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -15,12 +15,12 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
-
-# Add luci-theme-argon
-#git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon
-#rm -rf ../lean/luci-theme-argon
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Svn checkout packages from immortalwrt's repository
 git clone --depth=1 https://github.com/immortalwrt/packages -b openwrt-23.05 packages
 git clone --depth=1 https://github.com/immortalwrt/luci -b openwrt-23.05 luci
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
