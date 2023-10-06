1f2aceef724cdb363f3a3e052d09ad7fce526326
SamShum_June <38761558+SamShum-June@users.noreply.github.com>
2023-10-06 15:19:28 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -14,9 +14,22 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package'
+
 
 # Svn checkout packages from immortalwrt's repository
 git clone --depth=1 -b openwrt-23.05 https://github.com/immortalwrt/packages
 git clone --depth=1 -b openwrt-23.05 https://github.com/immortalwrt/luci
 
+#test
+# Add luci-theme-argon
+pushd ./luci/themes
+rm -rf ./luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
+pushd ./luci-theme-argon
+rm -rf ./RELEASE_ZH.md RELEASE.md README_ZH.md README.md LICENSE .gitignore .git .github
+popd
+popd
+
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld'
+sed -i '$a src-git openwrt-passwall https://github.com/xiaorouji/openwrt-passwall'
